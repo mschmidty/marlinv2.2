9c112d149c6fb7f27207a2bafd1ba1490cccb2ae
Michael Schmidt <mschmidty@gmail.com>
2014-06-21 13:50:32 -0600

    update to home page and fix column padding

--- home.php
+++ home.php
@@ -14,19 +14,19 @@ Template Name: Home Page
             <div class="content home">
                 <div class="grid">
                     <div class="col col-1-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/for-cause.png">
-                        <h3>For Your Cause</h3>
-                        <p>We want to give back to the community that supports us! A portion of our profits go to support local customer recommended causes.</p>
-                    </div>
-                    <div class="col col-1-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/colorado-flag.jpg">
-                        <h3>Local</h3>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/house.png">
+                        <h3>Free Estimates</h3>
                         <p>Durango Shade Company offers at home consultation for motorized and manual window shades.  Our shades are great for both commercial and residential applications.</p>
                     </div>
                     <div class="col col-1-3">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/phone.png">
-                        <h3>Give us a call!</h3>
-                        <p>Contact us by phone:970-497-9001 to set up a free consultation. We want to help you give your home that fresh new look.</p>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/colorado-flag.jpg">
+                        <h3>Professional Service</h3>
+                        <p>We take great pride in offering only quality products and professional services. We work hard to make your dream a reality.</p>
+                    </div>
+                    <div class="col col-1-3">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/for-cause.png">
+                        <h3>For Your Cause</h3>
+                        <p>Your purchase with Durango Shade Company will support the community. We donate a portion of our profits to customers recommended Causes.</p>
                     </div>
                 </div>
             </div>
